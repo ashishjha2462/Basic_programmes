4a1f202492a81148cce7d08af2f68acdb1efae3c
ashishjha2462 <96326894+ashishjha2462@users.noreply.github.com>
2022-12-08 19:47:23 +0530

    Update link1.c

--- link1.c
+++ link1.c
@@ -11,23 +11,23 @@ struct node *head;
 
 void insertatstart(int x)
 {
-    struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->data = x;
-    temp->next = head;
-    head = temp; 
+    struct node *temp = (struct node *)malloc(sizeof(struct node));// creating new node
+    temp->data = x;// storing data
+    temp->next = head;// attaching new node to previous linked list
+    head = temp; //reinitialising the head of the linked list
 }
 
 void insertatend(int x)
 {
-    struct node *temp1 = (struct node *)malloc(sizeof(struct node));
-    temp1->data = x;
-    temp1->next = NULL;
+    struct node *temp1 = (struct node *)malloc(sizeof(struct node));// creating new node
+    temp1->data = x;// storing data
+    temp1->next = NULL;// NULL is stored as next as it will be the last node
     struct node *temp2 = head;
-    while (temp2->next != NULL)
+    while (temp2->next != NULL)// traversing to the end of the list
     {
         temp2 = temp2->next;
     }
-    temp1->next = temp2->next;
+    temp1->next = temp2->next;// attaching new node to previous linked list
     temp2->next = temp1;
 }
 
@@ -38,14 +38,14 @@ static void reverse(struct node **head_ref)
     struct node *next = NULL;
     while (current != NULL)
     {
-        // Store next
-        next = current->next;
+        
+        next = current->next;// Store next
 
-        // Reverse current node's pointer
-        current->next = prev;
+        
+        current->next = prev;// Reverse current node's pointer
 
-        // Move pointers one position ahead.
-        prev = current;
+        
+        prev = current;// Move pointers one position ahead.
         current = next;
     }
     *head_ref = prev;
